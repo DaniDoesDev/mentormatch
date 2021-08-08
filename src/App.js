fd35035f39ae2ce6284b025673c5c13deb4bcd0d
Danielle <43801670+DaniDoesDev@users.noreply.github.com>
2021-08-07 22:31:54 -0500

    initialize and add button for oauth2 simple login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import OAuth2Login from 'react-simple-oauth2-login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const onSuccess = response => console.log(response);
+const onFailure = response => console.error(response);
+
+ReactDOM.render(
+  <OAuth2Login
+    authorizationUrl="https://accounts.spotify.com/authorize"
+    responseType="token"
+    clientId="9822046hvr4lnhi7g07grihpefahy5jb"
+    redirectUri="http://localhost:3000/oauth-callback"
+    onSuccess={onSuccess}
+    onFailure={onFailure}/>,
+  document.getElementById('root')
+);
 
 function App() {
   return (
@@ -7,15 +30,26 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The best way to connect with mentors. See yourself in your dream job today! 
         </p>
+        <li>
+              <Link to="/">Home</Link>
+            </li>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I'm a mentor!
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          I'm a mentee!
         </a>
       </header>
     </div>
