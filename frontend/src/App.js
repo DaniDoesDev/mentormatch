f670ed76af45376d1eb81ddaf8e8bbf43bdecd4a
Danielle <43801670+DaniDoesDev@users.noreply.github.com>
2021-08-09 11:11:55 -0500

    update oauth routing, separate sign up and log in routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,44 @@ const onFailure = response => console.error(response);
 
 var user = null;
 
+function mentorSignUp({code}) {
+  // Uses authorization code to make a POST request to get an access token,
+  // which then is exchanged for user info. from the github api
+    fetch("http://localhost:5000/github/token", {
+    method: 'POST',
+    body: JSON.stringify({code}),
+    headers: {
+      'content-type': 'application/json',
+    },
+    })
+    .then(res => res.json())
+    .then((data) => {
+      return data.access_token;
+    })
+    .then((token) => {
+      return fetch('https://api.github.com/user', {
+        method: 'GET',
+        headers: {
+          Authorization: "token "+token,
+        },
+      });
+    })
+    .then(res => res.json())
+    .then((data) => { // this is the user object returned by github's api
+      user = data;    // set global user 
+    });
+
+    // make get or post requests here to do stuff in db (store user info.)
+    // fetch('http://127.0.0.1:8000/api/mentor/', {
+    //   method = 'POST',
+      
+    // });
+    // console.log(user.name);
+
+    // go to mentor home page
+    return window.location.href="http://localhost:3000/mentorJobDetails";
+}
+
 function mentorLogin({code}) {
   // Uses authorization code to make a POST request to get an access token,
   // which then is exchanged for user info. from the github api
@@ -51,6 +89,46 @@ function mentorLogin({code}) {
 
     // go to mentor home page
     return window.location.href="http://localhost:3000/mentorHomePage";
+}
+
+
+function menteeSignUp({code}) {
+  // Uses authorization code to make a POST request to get an access token,
+  // which then is exchanged for user info. from the github api
+    fetch("http://localhost:5000/github/token", {
+    method: 'POST',
+    body: JSON.stringify({code}),
+    headers: {
+      'content-type': 'application/json',
+    },
+    })
+    .then(res => res.json())
+    .then((data) => {
+      return data.access_token;
+    })
+    .then((token) => {
+      return fetch('https://api.github.com/user', {
+        method: 'GET',
+        headers: {
+          Authorization: "token "+token,
+        },
+      });
+    })
+    .then(res => res.json())
+    .then((data) => { // this is the user object returned by github's api
+      user = data;    // set global user 
+    });
+
+    console.log(user);
+    // REMOVE ANY CONSOLE.LOG statements AFTER DEBUGGING IS OVER
+    // --> (it makes you reload the page again to go the next page instead)
+
+    // make get or post requests here to do stuff in db (store user info.)
+
+    // console.log(user.name);
+    
+    // go to mentee home page
+    return window.location.href="http://localhost:3000/menteeInterestPage";
 }
 
 function menteeLogin({code}) {
@@ -92,6 +170,7 @@ function menteeLogin({code}) {
     return window.location.href="http://localhost:3000/menteeHomePage";
 }
 
+
 export default function App() {
   /****************/
   /*** App Page ***/
@@ -112,7 +191,7 @@ export default function App() {
     clientId='16d350e690b51e39d0dd'
     clientSecret='24ecd4defbbb37e89904c714e91ff2bc3f636990'
     redirectUri='http://localhost:3000/'
-    onSuccess={mentorLogin}
+    onSuccess={mentorSignUp}
     onFailure={onFailureMentor}/>
       <p>
         
@@ -126,22 +205,49 @@ export default function App() {
     clientId='16d350e690b51e39d0dd'
     clientSecret='24ecd4defbbb37e89904c714e91ff2bc3f636990'
     redirectUri='http://localhost:3000/'
+    onSuccess={menteeSignUp}
+    onFailure={onFailureMentor}/>
+
+        <p></p>
+        <OAuth2Login
+    authorizationUrl='https://github.com/login/oauth/authorize'
+    responseType="code"
+    scope='read:user'
+    buttonText="Mentor Log In"
+    className="btn"
+    clientId='16d350e690b51e39d0dd'
+    clientSecret='24ecd4defbbb37e89904c714e91ff2bc3f636990'
+    redirectUri='http://localhost:3000/'
+    onSuccess={mentorLogin}
+    onFailure={onFailureMentor}/>
+      <p>
+        
+      </p>
+        <OAuth2Login
+    authorizationUrl='https://github.com/login/oauth/authorize'
+    responseType="code"
+    scope='read:user'
+    buttonText="Mentee Log In"
+    className="btn"
+    clientId='16d350e690b51e39d0dd'
+    clientSecret='24ecd4defbbb37e89904c714e91ff2bc3f636990'
+    redirectUri='http://localhost:3000/'
     onSuccess={menteeLogin}
     onFailure={onFailureMentor}/>
 
         <p></p>
         {/* This doesn't really do anything  */}
         {/* <button onClick={mentorLogin} className="btn">I'm a mentor!</button> */}
-        <Link to="/mentorJobDetails"><button className="btn">
+        {/* <Link to="/mentorJobDetails"><button className="btn">
            I'm a mentor!
          </button>
          </Link>
-        <p></p>
+        <p></p> */}
         {/* <button onClick={menteeLogin} className="btn">I'm a mentee!</button> */}
-        <Link to="/menteeInterestPage"><button className="btn">
+        {/* <Link to="/menteeInterestPage"><button className="btn">
            I'm a Mentee!
          </button>
-         </Link>
+         </Link> */}
       </header>
     </div>
   );
--- frontend/src/MenteeHomePage.js
+++ frontend/src/MenteeHomePage.js
@@ -23,8 +23,9 @@ export default class MenteeHomePage extends React.Component {
             <p>Here is your matched mentor's contact information. Reach out and start your journey in STEM today!</p>   
             <ul>
             { this.state.persons.slice(0,1).map(person => <p>{person.name}</p>)}
+            { this.state.persons.slice(0,1).map(person => <p>Company: {person.company.name}</p>)}
             { this.state.persons.slice(0,1).map(person => <p>{person.email}</p>)}
-            { this.state.persons.slice(0,1).map(person => <p>{person.phone}</p>)}
+            {/* { this.state.persons.slice(0,1).map(person => <p>{person.phone}</p>)} */}
             </ul>
             </div>
             )
